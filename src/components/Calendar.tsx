7f832c77b19a173755a8e929151c6616c18a8669
tiffany-slac <tiffany@slac.stanford.edu>
2026-04-16 16:37:54 -0700

    MNT: update calendar link with public permissions

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -3,7 +3,7 @@ export default function Calendar() {
     <div className="min-h-screen px-6 md:px-16 py-12">
       <div className="w-full h-[700px]">
         <iframe
-          src="https://calendar.google.com/calendar/embed?src=OTVjNzgxYjMyNmRmMzg2YjFlNzQzMTM1NDAzMjhkY2NlNDNiOWE3MWQ5OWQ4NDFhOWUxNzAyNjFiMzM2OGE0M0Bncm91cC5jYWxlbmRhci5nb29nbGUuY29t"
+          src="https://calendar.google.com/calendar/embed?src=YXV4d29ya3Nob3BAZ21haWwuY29t"
           className="w-full h-full border-0"
           loading="lazy"
         />
